a41fa021ffc47f07dd0b503657ea1da415ad49a6
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2024-10-26 17:56:56 -0600

    [Update]

--- lib/modules/auth/login_screen.dart
+++ lib/modules/auth/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:warehouse_master_mobile/kernel/widgets/form/text_form_field_email.dart';
+import 'package:warehouse_master_mobile/kernel/widgets/form/text_form_field_password.dart';
 import 'package:warehouse_master_mobile/styles/theme/app_theme.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,7 +14,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _username = TextEditingController();
   final TextEditingController _password = TextEditingController();
-  var _isObscure = true;
+  final _formKey = GlobalKey<FormState>(); // Clave del formulario
 
   @override
   Widget build(BuildContext context) {
@@ -28,122 +30,97 @@ class _LoginScreenState extends State<LoginScreen> {
           Center(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 50),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    elevation: 8,
-                    color: AppColors.lightGray,
-                    child: Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center, 
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/logo.png', width: 150, height: 150),
-                          const Text(
-                            '¡Bienvenido a WAREHOUSE!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: AppColors.deepRedAccent,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            'Ingrese su correo y contraseña para comenzar!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: AppColors.softPinkBackground,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 20), 
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Correo electrónico',
-                              label: const Text('Correo electrónico'),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      elevation: 8,
+                      color: AppColors.lightGray,
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center, 
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/logo.png', width: 150, height: 150),
+                            const Text(
+                              '¡Bienvenido a WAREHOUSE!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.deepRedAccent,
                               ),
                             ),
-                            keyboardType: TextInputType.emailAddress,
-                            controller: _username,
-                          ),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Contraseña',
-                              label: const Text('Contraseña'),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _isObscure = !_isObscure;
-                                  });
-                                },
-                                icon: Icon(
-                                  _isObscure
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Ingrese su correo y contraseña para comenzar!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.softPinkBackground,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                            keyboardType: TextInputType.text,
-                            controller: _password,
-                            obscureText: _isObscure,
-                          ),
-                          const SizedBox(height: 16),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text('¿Has olvidado tu contraseña?'),
-                              TextButton(
-                                onPressed: () {
-                                  print('Recuperar contraseña');
-                                },
-                                child: const Text(
-                                  'Recuperar!',
-                                  style: TextStyle(
-                                    color: AppColors.deepMaroon,
+                            const SizedBox(height: 20), 
+                            TextFieldEmail(
+                              controller: _username,
+                            ),
+                            const SizedBox(height: 16),
+                            TextFieldPassword(
+                              controller: _password,
+                            ),
+                            const SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('¿Has olvidado tu contraseña?'),
+                                TextButton(
+                                  onPressed: () {
+                                    print('Recuperar contraseña');
+                                  },
+                                  child: const Text(
+                                    'Recuperar!',
+                                    style: TextStyle(
+                                      color: AppColors.deepMaroon,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 16),
-                          SizedBox(
-                            width: double.infinity,
-                            height: 48,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                print('Email: ${_username.text}');
-                                print('Password: ${_password.text}');
-                              },
-                              style: OutlinedButton.styleFrom(
-                                foregroundColor: AppColors.palePinkBackground,
-                                backgroundColor: AppColors.rosePrimary,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
-                              ),
-                              child: const Text('Iniciar Sesión'),
+                              ],
                             ),
-                          ),
-                          
-                        ],
+                            const SizedBox(height: 16),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 48,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState?.validate() ?? false) {
+                                    print('Email: ${_username.text}');
+                                    print('Password: ${_password.text}');
+                                  }
+                                },
+                                style: OutlinedButton.styleFrom(
+                                  foregroundColor: AppColors.palePinkBackground,
+                                  backgroundColor: AppColors.rosePrimary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                ),
+                                child: const Text('Iniciar Sesión'),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
