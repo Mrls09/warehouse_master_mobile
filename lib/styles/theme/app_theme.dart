0e9be3e8f5005f438790e8fe28eae07ac18e4d0c
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2024-11-27 21:22:50 -0600

    [Udate] update color deepMarron

--- lib/styles/theme/app_theme.dart
+++ lib/styles/theme/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color rosePrimary = Color(0xFFBB5E7D); // Rosa
-  static const Color deepMaroon = Color(0xFF4B0019); // Marrón oscuro
+  static const Color deepMaroon = Color(0xCC4B0019); // Marrón oscuro
   static const Color softPinkBackground =
       Color(0xFFE196AF); // Fondo suave rosado
   static const Color palePinkBackground =
