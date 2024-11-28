03312015db4eb0034e797c49a756fdad7d5df88d
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2024-11-28 14:30:04 -0600

    [Update] Update custom dialog alert

--- lib/kernel/shared/custom_dialog_alert.dart
+++ lib/kernel/shared/custom_dialog_alert.dart
@@ -8,13 +8,24 @@ class CustomDialogAlert {
   Future<void> show({
     required Widget content,
     String? title,
+    Widget? icon,
     List<Widget>? actions,
   }) async {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: title != null ? Text(title) : null,
+          title: title != null
+              ? Row(
+                  children: [
+                    if (icon != null) ...[
+                      icon,
+                      const SizedBox(width: 8),
+                    ],
+                    Expanded(child: Text(title)),
+                  ],
+                )
+              : null,
           content: content,
           actions: actions,
         );
